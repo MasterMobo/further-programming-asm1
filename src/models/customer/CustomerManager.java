f2284dc476b9b3896bce8bf11e535742d23f7c83
MasterMobo <mastermobo101@gmail.com>
2024-04-04 13:36:36 +0700

    restructured customer managers

--- src/models/customer/CustomerManager.java
+++ src/models/customer/CustomerManager.java
@@ -1,9 +1,26 @@
 package models.customer;
 
-import models.id.IdManager;
+import models.customer.dependent.DependentManager;
+import models.customer.holder.PolicyHolderManager;
 
-import java.util.List;
+public interface CustomerManager {
 
-public interface CustomerManager<T extends Customer> extends IdManager<T> {
-    // This interface deals with all the customers in the system
+    // Generates a unique ID across all user roles.
+    // This method will NOT add the returned ID to the ID set.
+    // Use this method if you are only trying to generate a unique ID, but not use it to set the ID of a Customer
+    // If you want to use the return value to set a Customer ID, use addID() after setting it.
+    // Alternatively, use generateAndAddId() to accomplish this.
+    String generateId();
+
+    // Adds an ID to the ID set.
+    // This method should ALWAYS be used in tandem with generateId() if you are trying to set Customer ID.
+    void addId(String id);
+
+    // Generates a unique ID then add it to the ID set.
+    // This method should NOT be used to exclusively create Customer IDs. Doing so will not ensure uniqueness on subsequent calls.
+    // Use this method ONLY if you are sure that the returned ID will be used to set Customer ID.
+    String generateAndAddId();
+
+    PolicyHolderManager getPolicyHolders();
+    DependentManager getDependents();
 }
---
+++ src/models/customer/CustomerRoleManager.java
@@ -0,0 +1,7 @@
+package models.customer;
+
+import models.id.IdManager;
+
+public interface CustomerRoleManager<T extends Customer> extends IdManager<T> {
+    // This interface deals with all the customers in the system
+}
