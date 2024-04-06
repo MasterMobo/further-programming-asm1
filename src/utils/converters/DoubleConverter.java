e1919e5d492bc8c013f0ed98150f3e097895e406
MasterMobo <mastermobo101@gmail.com>
2024-04-06 19:51:45 +0700

    added more trim

--- src/utils/converters/DoubleConverter.java
+++ src/utils/converters/DoubleConverter.java
@@ -4,6 +4,6 @@ public class DoubleConverter extends TypeConverter<Double>{
 
     @Override
     public Double fromString(String s) {
-        return Double.parseDouble(s);
+        return Double.parseDouble(s.trim());
     }
 }
